98edc24c64ef0735fc9cb3063acb012115650202
Pawel Banasiak <100700195+pawelbanasiak@users.noreply.github.com>
2023-02-07 14:58:53 +0100

    1.0.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pawelbanasiak/test1
+
+go 1.19
+
+require github.com/pawelbanasiak/test2 v1.0.0 // indirect
---
+++ test2.go
@@ -0,0 +1,12 @@
+package test1
+
+import (
+	"fmt"
+
+	"github.com/pawelbanasiak/test2"
+)
+
+func S() {
+	fmt.Println("from test1:")
+	test2.S()
+}
